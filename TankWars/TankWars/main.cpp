d0ce36cb4dce4715383228f47c79f03e24f885a5
Thanos Panagiotidis <thanos.panagiotidis@protonmail.com>
2024-12-23 15:46:23 +0200

    Added player sprite

--- TankWars/TankWars/main.cpp
+++ TankWars/TankWars/main.cpp
@@ -13,53 +13,56 @@ struct GameState
 
 	float m_player_pos_x = m_canvas_width / 2.0f;
 	float m_player_pos_y = m_canvas_height / 2.0f;
-	float m_player_size_x = 1.0f;
-	float m_player_size_y = 1.0f;
+	float m_player_size_x = 0.78f;  // x is 2.62 times the y
+	float m_player_size_y = 0.3f;
 	float m_cannon_degrees = 0.0f;
 
 	// float m_global_offset_x = 0.0f;
 	// float m_global_offset_y = 0.0f;
 };
 
-GameState state;
+GameState global_state;
 
 void init()
 {
-	SETCOLOR(state.m_brush_player.fill_color, 1.0f, 1.0f, 1.0f);
-	state.m_brush_player.outline_opacity = 0.0f;
+	SETCOLOR(global_state.m_brush_background.fill_color, 1.0f, 1.0f, 1.0f);
+	global_state.m_brush_background.fill_opacity = 1.0f;
+	global_state.m_brush_background.outline_opacity = 0.0f;
+	global_state.m_brush_background.texture = global_state.m_asset_path + "background.png";
 
-	SETCOLOR(state.m_brush_background.fill_color, 1.0f, 1.0f, 1.0f);
-	state.m_brush_background.fill_opacity = 1.0f;
-	state.m_brush_background.outline_opacity = 0.0f;
-	state.m_brush_background.texture = state.m_asset_path + "background.png";
+	SETCOLOR(global_state.m_brush_player.fill_color, 1.0f, 1.0f, 1.0f);
+	SETCOLOR(global_state.m_brush_player.outline_color, 1.0f, 0.3f, 0.1f);
+	global_state.m_brush_player.fill_opacity = 1.0f;
+	global_state.m_brush_player.outline_opacity = 0.0f;
+	global_state.m_brush_player.texture = global_state.m_asset_path + "tank_left.png";
 }
 
 void draw()
 {
-	graphics::drawRect(state.m_canvas_width / 2.0f, state.m_canvas_height / 2.0f, state.m_canvas_width, state.m_canvas_height, state.m_brush_background);
+	graphics::drawRect(global_state.m_canvas_width / 2.0f, global_state.m_canvas_height / 2.0f, global_state.m_canvas_width, global_state.m_canvas_height, global_state.m_brush_background);
 
-	graphics::drawRect(state.m_player_pos_x, state.m_player_pos_y, state.m_player_size_x, state.m_player_size_y, state.m_brush_player);
+	graphics::drawRect(global_state.m_player_pos_x, global_state.m_player_pos_y, global_state.m_player_size_x, global_state.m_player_size_y, global_state.m_brush_player);
 }
 
 void update(float dt)
 {
 	// float p = 1.0f + fabs(cos(graphics::getGlobalTime() / 2000.0f)) * 0.8f;
-	// SETCOLOR(state.m_brush_background.fill_color, 2.4f * p, 0.06f * p, 0.06f * p);
+	// SETCOLOR(global_state.m_brush_background.fill_color, 2.4f * p, 0.06f * p, 0.06f * p);
 
 	float delta_time = dt / 1000.0f;
-	const float velocity = 10.0f;
+	const float velocity = 0.8f;
 
 	if (graphics::getKeyState(graphics::SCANCODE_A))
-		state.m_player_pos_x -= velocity * delta_time;
+		global_state.m_player_pos_x -= velocity * delta_time;
 	if (graphics::getKeyState(graphics::SCANCODE_D))
-		state.m_player_pos_x += velocity * delta_time;
-	if (graphics::getKeyState(graphics::SCANCODE_W) and state.m_cannon_degrees < 90.0f)
-		state.m_cannon_degrees += velocity * delta_time;
-	if (graphics::getKeyState(graphics::SCANCODE_S) and state.m_cannon_degrees > 0.0f)
-		state.m_cannon_degrees -= velocity * delta_time;
+		global_state.m_player_pos_x += velocity * delta_time;
+	if (graphics::getKeyState(graphics::SCANCODE_W) and global_state.m_cannon_degrees < 90.0f)
+		global_state.m_cannon_degrees += velocity * delta_time;
+	if (graphics::getKeyState(graphics::SCANCODE_S) and global_state.m_cannon_degrees > 0.0f)
+		global_state.m_cannon_degrees -= velocity * delta_time;
 
-	// state.m_global_offset_x = state.m_canvas_width / 2.0f - state.m_player_pos_x;
-	// state.m_global_offset_y = state.m_canvas_height / 2.0f - state.m_player_pos_y;
+	// global_state.m_global_offset_x = global_state.m_canvas_width / 2.0f - global_state.m_player_pos_x;
+	// global_state.m_global_offset_y = global_state.m_canvas_height / 2.0f - global_state.m_player_pos_y;
 }
 
 int main(int argc, char** argv)
@@ -71,7 +74,7 @@ int main(int argc, char** argv)
 	graphics::setDrawFunction(draw);
 	graphics::setUpdateFunction(update);
 
-	graphics::setCanvasSize(state.m_canvas_width, state.m_canvas_height);
+	graphics::setCanvasSize(global_state.m_canvas_width, global_state.m_canvas_height);
 	graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
 
 	graphics::startMessageLoop();
